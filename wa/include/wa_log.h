5f52691dd5d746855a672ddeebd86b9f88e1b2cb
laxyy69 <vrekmakerq@gmail.com>
2024-02-06 21:23:08 +0200

    Updated wa_log

--- wa/include/wa_log.h
+++ wa/include/wa_log.h
@@ -5,6 +5,7 @@ enum wa_log_level
 {
     WA_FATAL,
     WA_ERROR,
+    WA_WARN,
     WA_INFO,
     WA_DEBUG,
 
@@ -12,6 +13,13 @@ enum wa_log_level
 };
 
 void wa_log_set_level(enum wa_log_level level);
-void wa_log(enum wa_log_level level, const char* format, ...);
+void wa_log_msg(enum wa_log_level level, const char* file, int line, const char* format, ...);
+
+/*
+ * wa_logf() will log the file name and line number with the message
+ * wa_log() will just log a message
+ */
+#define wa_logf(level, format, ...) wa_log_msg(level, __FILE_NAME__, __LINE__, format, ##__VA_ARGS__)
+#define wa_log(level, format, ...) wa_log_msg(level, NULL, 0, format, ##__VA_ARGS__)
 
 #endif // _WA_LOG_H_
--- wa/src/wa_log.c
+++ wa/src/wa_log.c
@@ -3,27 +3,32 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-#define WA_LOG_LEN 256
+#define WA_LOG_LEN 4096
 
 /* Terminal colors */
 #define FG_RED "\033[91m"
 #define BG_RED "\033[101m"
 #define FG_GREEN "\033[92m"
+#define FG_ORANGE "\033[93m"
 #define FG_DEFAULT "\033[39m"
 #define BG_DEFAULT "\033[49m"
+#define FG_BLACK "\033[30m"
 
 #define C_DEFAULT FG_DEFAULT BG_DEFAULT
+#define C_FATAL BG_RED FG_BLACK
 
 static const char* wa_log_str[WA_LOG_LEVEL_LEN] = {
     "WA [" FG_RED "FATAL" FG_DEFAULT "]",
     "WA [" FG_RED "ERROR" FG_DEFAULT "]",
+    "WA [" FG_ORANGE "WARN" FG_DEFAULT "]",
     "WA [" FG_GREEN "INFO" FG_DEFAULT "]",
     "WA [DEBUG]",
 };
 
 static const char* wa_log_color[WA_LOG_LEVEL_LEN] = {
-    BG_RED,             /* WA_FATAL */
+    C_FATAL,            /* WA_FATAL */
     FG_RED,             /* WA_ERROR */
+    FG_DEFAULT,         /* WA_WARN */
     FG_DEFAULT,         /* WA_INFO */
     FG_DEFAULT          /* WA_DEBUG */
 };
@@ -35,13 +40,13 @@ void wa_log_set_level(enum wa_log_level level)
     wa_level = level;
 }
 
-void wa_log(enum wa_log_level level, const char* format, ...)
+void wa_log_msg(enum wa_log_level level, const char* filename, int line, const char* format, ...)
 {
     if (wa_level < level)
         return;
 
     char output[WA_LOG_LEN];
-    FILE* file = (level <= WA_ERROR) ? stderr : stdout;
+    FILE* file = (level <= WA_WARN) ? stderr : stdout;
 
     va_list args;
     va_start(args, format);
@@ -50,7 +55,10 @@ void wa_log(enum wa_log_level level, const char* format, ...)
 
     va_end(args);
 
-    fprintf(file, "%s:\t%s", wa_log_str[level], wa_log_color[level]);
+    if (filename)
+        fprintf(file, "%s [%s:%d]:\t%s", wa_log_str[level], filename, line, wa_log_color[level]);
+    else
+        fprintf(file, "%s:\t%s", wa_log_str[level], wa_log_color[level]);
 
     fprintf(file, "%s%s%s", wa_log_color[level], output, C_DEFAULT);
 
