e114dc42f058caa705014b7b7b5a89746a06ed0b
laxyyza <laxyy420@proton.me>
2024-08-21 18:33:50 +0200

    tests/simple: Set fullscreen from command line

--- tests/simple.c
+++ tests/simple.c
@@ -1,8 +1,14 @@
 #include <wa.h>
+#include <string.h>
 
-int main(void)
+int main(int argc, const char** argv)
 {
-    wa_window_t* window = wa_window_create("WA Simple Window", 640, 480, false);
+    bool fullscreen = false;
+    if (argc == 2)
+        if (strcmp(argv[1], "fullscreen") == 0)
+            fullscreen = true;
+
+    wa_window_t* window = wa_window_create("WA Simple Window", 640, 480, fullscreen);
     wa_print_opengl();
 
     wa_window_mainloop(window);
