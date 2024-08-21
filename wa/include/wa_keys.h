6a07d51ada16c65d928fa073c5b3205627488f95
laxyyza <laxyy420@proton.me>
2024-08-22 00:12:57 +0200

    wa_keys: Add WA_KEY_V

--- wa/include/wa_keys.h
+++ wa/include/wa_keys.h
@@ -6,12 +6,14 @@
 
 #define WA_KEY_F XKB_KEY_f
 #define WA_KEY_R XKB_KEY_r
+#define WA_KEY_V XKB_KEY_v
 #endif // _WA_WAYLAND
 
 #ifdef _WA_WIN32
 
 #define WA_KEY_F 'F'
 #define WA_KEY_R 'R'
+#define WA_KEY_V 'V'
 #endif // _WA_WIN32
 
 #endif
